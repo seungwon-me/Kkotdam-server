88d712e1e89cdaa1d99cce73d5312212aadcd4bd
Seung Won <tmddnjsrla99@gmail.com>
2025-06-29 21:46:30 +0900

    upload project

---
+++ src/main/kotlin/me/seungwon/kkotdam/Application.kt
@@ -0,0 +1,11 @@
+package me.seungwon.kkotdam
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KkotdamApplication
+
+fun main(args: Array<String>) {
+    runApplication<KkotdamApplication>(*args)
+}
---
+++ src/test/kotlin/me/seungwon/kkotdam/KkotdamApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.seungwon.kkotdam
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KkotdamApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
